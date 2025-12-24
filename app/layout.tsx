7d2c0408cf804100ca0921f4c8df6f1d8678dbf3
ROHIT_YERUNKAR_08 <rohityerunkar1508@gmail.com>
2025-12-24 12:05:07 +0530

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,20 +23,6 @@ export const metadata: Metadata = {
       { url: "/favicon-32x32.png", sizes: "32x32", type: "image/png" },
     ],
     apple: "/apple-touch-icon.png",
-    other: [
-      {
-        rel: "icon",
-        url: "/android-chrome-192x192.png",
-        sizes: "192x192",
-        type: "image/png",
-      },
-      {
-        rel: "icon",
-        url: "/android-chrome-512x512.png",
-        sizes: "512x512",
-        type: "image/png",
-      },
-    ],
   },
 };
 
